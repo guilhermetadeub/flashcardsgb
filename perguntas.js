0f56be934bab4d907065354a7ae25eda96122a56
guilhermetadeub <tadeuguilherme175@gmail.com>
2024-12-01 02:10:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+    'Matemática',
+    'Qual é a raiz de 144?',
+    ' resposta é 12.'
+)
+
+criaCartao(
+    'História',
+    'Quando descobriram o Brasil?',
+    'A resposta é 22/04/1500.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país em extensão territorial do mundo?',
+    'Rússia'
+)
+
+criaCartao(
+    'Curiosidades Gerais',
+    'Quantos segundos existem em um dia?',
+    '86.400 segundos.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 3x8-9?',
+    'resposta é 15.'
+)
+
+
